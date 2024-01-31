ab9cb9f80694901e9ef4522a0aaf17fad9f6fd28
Belghalimohammed <belghalimohammed0@gmail.com>
2024-01-31 18:54:47 +0000

    project split 2

--- backend/services/AppService.py
+++ backend/services/AppService.py
@@ -9,20 +9,21 @@ class AppService():
     def __init__(self,iotdao,enddao) -> None:
         self.iotdao:IotDao = iotdao
         self.enddao:EndDao = enddao
-        
+        self.lock = multiprocessing.Lock()
     def hundle(self):
         
         while True:
             ds=self.iotdao.getAllDevices()
             ed=self.enddao.getAllEndDevices()
-            for device in ds:
-                self.iotdao.hundle(device)
+            with self.lock:
+                for device in ds:
+                    self.iotdao.hundle(device)
 
 
 
-            
-            for dv in ed:
-                self.enddao.hundle(dv)   
+            with self.lock:
+                for dv in ed:
+                    self.enddao.hundle(dv)   
 
             sleep(10)
 
